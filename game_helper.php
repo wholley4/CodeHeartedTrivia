83f4ffd251a81e6ab71c905ebeb7086f21039704
KalilTheNerd <kalil4612@yahoo.com>
2020-03-30 16:45:58 -0400

    Helper-select page created. Need to add link to next game depending on the character they select.

---
+++ game_helper.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./CHTglike.css" title="style" />
+    </head>
+    <body>
+        <h1 style="margin-bottom:10px;margin-top:0px;"> Pick your CS Helper </h1>
+        <div>
+            <table class="helper">
+                <tr style="height:50px;">
+                    <td rowspan="2"> <img src="./project2/kid1.png" class="helper_picture">
+                    </td>
+                    <td> 
+                        <h2 style="margin-bottom: 0px;">Taylor</h2>
+                        <i>User Interface</i>
+                    </td>
+                </tr>
+            </table>
+            <table class="helper">
+                <tr style="height:50px;">
+                    <td rowspan="2"> <img src="./project2/kid2.png" class="helper_picture">
+                    </td>
+                    <td> 
+                        <h2 style="margin-bottom: 0px;">Prime</h2>
+                        <i>Hardware</i>
+                    </td>
+                </tr>
+            </table>
+            <table class="helper">
+                <tr style="height:50px;">
+                    <td rowspan="2"> <img src="./project2/kid3.png" class="helper_picture">
+                    </td>
+                    <td> 
+                        <h2 style="margin-bottom: 0px;">Ben</h2>
+                        <i>Requirements</i>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div style="clear:left;">
+            <table class="helper">
+                <tr style="height:50px;">
+                    <td rowspan="2"> <img src="./project2/kid4.png" class="helper_picture">
+                    </td>
+                    <td> 
+                        <h2 style="margin-bottom: 0px;">Roman</h2>
+                        <i>Security</i>
+                    </td>
+                </tr>
+            </table>
+            <table class="helper">
+                <tr style="height:50px;">
+                    <td rowspan="2"> <img src="./project2/kid5.png" class="helper_picture">
+                    </td>
+                    <td> 
+                        <h2 style="margin-bottom: 0px;">Sarah</h2>
+                        <i>Logic</i>
+                    </td>
+                </tr>
+            </table>
+            <table class="helper">
+                <tr style="height:50px;">
+                    <td rowspan="2"> <img src="./project2/kid6.png" class="helper_picture">
+                    </td>
+                    <td> 
+                        <h2 style="margin-bottom: 0px;">Sruti</h2>
+                        <i>Data Structures</i>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
